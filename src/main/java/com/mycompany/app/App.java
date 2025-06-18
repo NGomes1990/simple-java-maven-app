9d07cee7280b91541104b2fc6b7e13ff07613ea0
NGomes1990 <93449942+NGomes1990@users.noreply.github.com>
2025-06-18 15:55:41 +0100

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,24 +1,25 @@
 package com.mycompany.app;
 
+import java.util.logging.Logger;
+
 /**
  * Hello world!
  */
 public class App {
-     Logger logger = Logger.getLogger(getClass().getName());
-
-
+    // Make logger static since it's used in static main method
+    private static final Logger logger = Logger.getLogger(App.class.getName());
     private static final String MESSAGE = "Hello World!";
 
     public App() {}
 
     public static void main(String[] args) {
+        logger.info("Starting application...");
         System.out.println(MESSAGE);
-        logger.info("My Message");  // Compliant, output via logger
+        logger.info("My Message");
     }
 
     public String getMessage() {
+        logger.info("Getting message...");
         return MESSAGE;
-        System.out.println(MESSAGE);
-        logger.info("My Message");  // Compliant, output via logger
     }
 }
